ad0524f6ce56004376a0d082cd5f99ecdc2d5e46
acharton <antoine.charton@gmail.com>
2018-01-06 00:04:14 -0500

    added OSC sender

--- Constellation/Assets/Plugins/Constellation/Scripts/NodeFactory/ExperimentalNodeFactory.cs
+++ Constellation/Assets/Plugins/Constellation/Scripts/NodeFactory/ExperimentalNodeFactory.cs
@@ -5,9 +5,12 @@ namespace Constellation.Experimental {
                 case OSCManager.NAME:
                     INode nodeOsc = new OSCManager () as INode;
                     return new Node<INode> (nodeOsc);
-                case OSCFilter.NAME:
-                    INode nodeOSCFilter = new OSCFilter () as INode;
+                case OSCReceive.NAME:
+                    INode nodeOSCFilter = new OSCReceive () as INode;
                     return new Node<INode> (nodeOSCFilter);
+                case OSCSend.NAME:
+                    INode nodeOSCSend = new OSCSend () as INode;
+                    return new Node<INode> (nodeOSCSend);
                 default:
                     return null;
             }
