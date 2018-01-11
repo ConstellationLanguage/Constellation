4e85bcef66571cc3c528e5a72e1f6088d1339bff
acharton <antoine.charton@gmail.com>
2018-01-10 20:37:13 -0500

    Hudge refactoring of the editor. Change folder place. Removed  ugly try catch to avoid layout mismatch exception.

---
+++ Constellation/Assets/Constellation/Editor/ConstellationEditorBase.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace ConstellationEditor
+{
+    public class ExtendedEditorWindow : EditorWindow, IGUI
+    {
+        public bool shouldRepaint = false;
+        public void DragWindow()
+        {
+            GUI.DragWindow();
+        }
+
+        public void SetColor(Color color)
+        {
+            GUI.color = color;
+        }
+
+        public float VerticalScrollBar(Rect position, float value, float size, float topValue, float bottomValue)
+        {
+            return GUI.VerticalScrollbar(position, value, 1.0F, topValue, bottomValue);
+        }
+
+        public float HorizontalScrollBar(Rect position, float value, float size, float topValue, float bottomValue)
+        {
+            return GUI.HorizontalScrollbar(position, value, 1.0F, topValue, bottomValue);
+        }
+
+        public void DrawTexture(Rect rect, Texture2D texture)
+        {
+            GUI.DrawTexture(rect, texture);
+        }
+
+        public void  RequestRepaint()
+        {
+            shouldRepaint = true;
+        }
+
+        public bool DrawButton(Rect _rect)
+        {
+            if (GUI.Button(_rect, ""))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
