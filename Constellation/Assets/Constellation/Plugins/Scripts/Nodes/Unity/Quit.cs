1438493aba74c04cdc7bb86774e3db8a27a998eb
acharton <antoine.charton@gmail.com>
2018-05-26 14:45:02 -0400

    Added quit node

---
+++ Constellation/Assets/Constellation/Plugins/Scripts/Nodes/Unity/Quit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Constellation.Unity {
+	public class Quit : INode, IReceiver {
+		public const string NAME = "Quit";
+		UnityEngine.GameObject GameObject;
+		ISender sender;
+		public void Setup (INodeParameters _nodeParameters) {
+			_nodeParameters.AddInput (this, false, "Quit application");
+			sender = _nodeParameters.GetSender();
+		}
+
+		public string NodeName () {
+			return NAME;
+		}
+
+		public string NodeNamespace () {
+			return NameSpace.NAME;
+		}
+
+		public void Set (GameObject _gameObject) {
+			GameObject = _gameObject;
+		}
+
+		public void Receive (Variable _value, Input _input) { 
+			Application.Quit();
+		}
+	}
+}
