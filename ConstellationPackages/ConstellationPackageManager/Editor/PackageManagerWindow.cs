8661b4b31c3a67c9da42eaa7952854053492cac2
Antoine Charton <antoine.charton@gmail.com>
2020-08-23 23:56:58 -0400

    Added package installer

---
+++ ConstellationPackages/ConstellationPackageManager/Editor/PackageManagerWindow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PackageManagerWindow : EditorWindow
+{
+
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Window/Constellation Package Manager")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        PackageManagerWindow window = (PackageManagerWindow)EditorWindow.GetWindow(typeof(PackageManagerWindow));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Choose your version");
+    }
+}
---
+++ PackageInstaller/Assets/ConstellationPackageInstaller/Editor/InstallerWindow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class InstallerWindow : EditorWindow
+{
+
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Window/Constellation Installer")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        InstallerWindow window = (InstallerWindow)EditorWindow.GetWindow(typeof(InstallerWindow));
+        window.Show();
+        window.maxSize = new Vector2(200, 200);
+        window.minSize = new Vector2(200, 200);
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("This utility will install \n Constellation package manager.");
+        if (GUILayout.Button("Web site"))
+        {
+            Application.OpenURL("http://constellationeditor.com/");
+        }
+        
+        if (GUILayout.Button("Github"))
+        {
+            Application.OpenURL("https://github.com/ConstellationLanguage/Constellation");
+        }
+
+        GUILayout.Space(EditorGUIUtility.singleLineHeight);
+        GUILayout.Label("Requirements:");
+        if (GUILayout.Button("Git"))
+        {
+            Application.OpenURL("https://git-scm.com/");
+        }
+
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("Install"))
+        {
+
+        }
+    }
+}
