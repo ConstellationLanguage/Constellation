bc3ab973c77ae73295b91e0afa03930986598973
Antoine Charton <antoine.charton@gmail.com>
2020-08-24 02:11:07 -0400

    Added delete Installer on complete

--- PackageInstaller/Assets/ConstellationPackageInstaller/Editor/InstallerWindow.cs
+++ PackageInstaller/Assets/ConstellationPackageInstaller/Editor/InstallerWindow.cs
@@ -117,6 +117,7 @@ public class InstallerWindow : EditorWindow
             GUI.color = yellow;
             if (GUILayout.Button("Finish", GUILayout.Height(EditorGUIUtility.singleLineHeight)))
             {
+                FileUtil.DeleteFileOrDirectory("yourPath/YourFileOrFolder");
                 this.Close();
             }
         }
